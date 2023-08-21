c8e0a087752a3061f424ca904c40d678f4732617
shuai_yang <shuai_yang@intsig.net>
2023-08-21 19:18:45 +0800

    fix

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tally/themes/dart.dart';
 import 'package:tally/themes/light.dart';
@@ -6,23 +7,36 @@ import 'package:themed/themed.dart';
 
 class SettingScreen extends ConsumerWidget {
   const SettingScreen({super.key});
-  
+
   Map<ThemeRef, Object>? get anotherTheme => null;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
-      child: Switch(
-        value: Themed.ifCurrentThemeIs(darkTheme),
-        onChanged: (value) {
-          if (Themed.ifCurrentThemeIs(darkTheme)) {
-            Themed.clearCurrentTheme();
-          } else {
-            print('set dark theme: $darkTheme');
-            Themed.currentTheme = darkTheme;
-          }
-        },
-      )
-    );
+        child: SizedBox(
+      height: 89.h,
+      child: Column(
+        children: [
+          Text(
+            '切换主题',
+            style: TextStyle(
+              color: LightTheme.darkBlack,
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Switch(
+            value: Themed.ifCurrentThemeIs(darkTheme),
+            onChanged: (value) {
+              if (Themed.ifCurrentThemeIs(darkTheme)) {
+                Themed.clearCurrentTheme();
+              } else {
+                Themed.currentTheme = darkTheme;
+              }
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
